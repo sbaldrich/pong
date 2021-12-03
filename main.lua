4661cf8862fbc89ae6abae84ff8e62a80ac94451
Santiago Baldrich <santiago.baldrich@gmail.com>
2021-12-03 21:21:29 +0100

    The low-res update

--- main.lua
+++ main.lua
@@ -1,8 +1,19 @@
-WINDOW_WIDTH = 1280;
-WINDOW_HEIGHT = 720;
+-- push allows to draw to the screen in a virtual resolution, which will help
+-- transmit a more retro aesthetic
+-- https://github.com/Ulydev/push (not actively maintained as of Dec 2021)
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -10,22 +21,31 @@ function love.load()
 end
 
 --[[
+    Having to click the x to quit is a drag. Let the player use the ESC key for that
+]]
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+--[[
     Called by LÖVE2D on every frame to redraw the screen.
     ]]
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
-        'center')               -- alignment mode, can be 'center', 'left', or 'right'
-end
+    -- start rendering at virtual resolution
+    push:apply('start')
 
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
+end
 
 --[[
     dt will always be a fraction of a second, this function is used by LÖVE2D so
         you can make any required changes to the state of the game.
 ]]
 function love.update(dt)
--- Nothing yet!
+    -- Nothing yet!
 end
