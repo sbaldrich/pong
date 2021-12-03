787622becd55cbc0b321ecdb0730c1c8c933cebf
Santiago Baldrich <santiago.baldrich@gmail.com>
2021-12-03 21:46:26 +0100

    The paddle update

--- main.lua
+++ main.lua
@@ -9,13 +9,17 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     -- This is a more "retro-looking" font that can be used ...
-    local smallFont = love.graphics.newFont('font.ttf', 8)
+    smallFont = love.graphics.newFont('font.ttf', 8)
 
+    -- larger font for drawing the score on the screen
+    scoreFont = love.graphics.newFont('font.ttf', 32)
     -- ... here. After setting it as the current font.
     love.graphics.setFont(smallFont)
 
@@ -24,6 +28,12 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
 
 --[[
@@ -43,15 +53,24 @@ function love.draw()
     push:apply('start')
 
     -- Set the background to a color similar to the original Pong
-    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 
+    love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- draw score on the left and right center of the screen
+    -- need to switch font to draw before actually printing
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, 
+        VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
+        VIRTUAL_HEIGHT / 3)
 
     -- Draw the paddles and the ball
     -- left
-    love.graphics.rectangle('fill', 10, 30, 5, 20);
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20);
     -- right
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5 , 20);
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20);
 
     -- ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4);
@@ -65,5 +84,18 @@ end
         you can make any required changes to the state of the game.
 ]]
 function love.update(dt)
-    -- Nothing yet!
+    if love.keyboard.isDown('w') then
+        -- No syntactic sugar for adding and assigning at the same time
+        player1Y = player1Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        -- No syntactic sugar for adding and assigning at the same time
+        player2Y = player2Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+
 end
